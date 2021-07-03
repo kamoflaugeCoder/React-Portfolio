f70f5015e188df663a530a71c1fd718029b84150
tjcrowell <mrcrowell79@gmail.com>
2021-07-03 00:44:36 -0400

    Progress bars added to my skills

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,7 +9,15 @@ function AboutPage() {
             <Title title={`About Me`} span={`About Me`} />
             <ImageSection />
             <Title title={`My Skills`} span={`My Skills`} />
-            <SkillsSection skill={'Javascript'} progress={'70%'} />
+            <div className="skills-container">
+                <SkillsSection skill={'Javascript'} progress={'50%'} width={'50%'} />
+                <SkillsSection skill={'Typescript'} progress={'40%'} width={'40%'} />
+                <SkillsSection skill={'React Js'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Node Js'} progress={'20%'} width={'20%'} />  
+                <SkillsSection skill={'HTML'} progress={'10%'} width={'10%'} />
+                <SkillsSection skill={'CSS'} progress={'100%'} width={'100%'} />
+                <SkillsSection skill={'GitHub'} progress={'60%'} width={'60%'} />
+            </div>
         </div>
     )
 }
